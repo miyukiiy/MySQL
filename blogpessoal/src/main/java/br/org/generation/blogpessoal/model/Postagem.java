76ce368e1c7f1d46961fe8995eafa5385b240ffc
Jessica <jehssicamiyuki@gmail.com>
2021-08-24 20:21:45 -0300

    Realizado em 24.08

---
+++ blogpessoal/src/main/java/br/org/generation/blogpessoal/model/Postagem.java
@@ -0,0 +1,61 @@
+package br.org.generation.blogpessoal.model;
+
+import java.util.Date;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Entity
+@Table(name = "tb_postagens")
+public class Postagem {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@NotNull(message = "O atriuto títuo é obrigatorio")
+	@Size(min = 5, max = 100, message = "O atributo deve ter no mínimo 5 e no máximo 100 carcteres")
+	private String titulo;
+	
+	@NotNull(message = "O atriuto texto é obrigatorio")
+	@Size(min = 5, max = 1000, message = "O atributo deve ter no mínimo 5 e no máximo 1000 carcteres")
+	private String texto;
+	
+	@Temporal(TemporalType.TIMESTAMP)
+	private Date data = new java.sql.Date(System.currentTimeMillis());
+	
+	
+	public long getId() {
+		return id;
+	}
+	public void setId(long id) {
+		this.id = id;
+	}
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getTexto() {
+		return texto;
+	}
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+	public Date getData() {
+		return data;
+	}
+	public void setData(Date data) {
+		this.data = data;
+	}
+	
+	
+}
